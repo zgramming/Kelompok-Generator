240dd4af165358b6ae6cc2eaa0ab80af1755f44a
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-11-05 01:51:57 +0700

    create global toggle view to list/gridview

--- lib/src/providers/global/global_provider.dart
+++ lib/src/providers/global/global_provider.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+enum ToggleView { ListView, GridView }
+
 /// In rare case , this variable usefull when context on widget is dispose
 /// Usually initialize this variable in screen after login [WelcomeScreen/HomeScreen]
 final globalContext = StateProvider<BuildContext>((ref) => null);
@@ -8,4 +10,8 @@ final globalContext = StateProvider<BuildContext>((ref) => null);
 /// For loading builder in ProviderListener
 final globalLoading = StateProvider.autoDispose<bool>((ref) => false);
 
+/// For key animated list
 final globalAnimatedListKey = StateProvider<GlobalKey<AnimatedListState>>((ref) => null);
+
+/// for toggle between list / grid veiw
+final globalToggleView = StateProvider((ref) => ToggleView.GridView);
