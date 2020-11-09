820af02385cbee302fb1ae0690b3872ba3f32cbc
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-11-09 17:13:20 +0700

    add global provider name group

--- lib/src/providers/global/global_provider.dart
+++ lib/src/providers/global/global_provider.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-enum ToggleView { ListView, GridView }
-
 /// In rare case , this variable usefull when context on widget is dispose
 /// Usually initialize this variable in screen after login [WelcomeScreen/HomeScreen]
 final globalContext = StateProvider<BuildContext>((ref) => null);
@@ -13,5 +11,5 @@ final globalLoading = StateProvider.autoDispose<bool>((ref) => false);
 /// For key animated list
 final globalAnimatedListKey = StateProvider<GlobalKey<AnimatedListState>>((ref) => null);
 
-/// for toggle between list / grid veiw
-final globalToggleView = StateProvider((ref) => ToggleView.GridView);
+/// For passing name group between screen
+final globalNameGroup = StateProvider<String>((ref) => 'Kelompok Generator');
