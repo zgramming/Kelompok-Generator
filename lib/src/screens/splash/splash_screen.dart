a27065083d73c54674acebc41cdc002bdb3ddd77
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-02-27 23:03:17 +0700

    adding condition for onboarding screen

--- lib/src/screens/splash/splash_screen.dart
+++ lib/src/screens/splash/splash_screen.dart
@@ -3,6 +3,7 @@ import 'package:global_template/global_template.dart';
 import 'package:flutter/material.dart';
 
 import '../../providers/providers.dart';
+import '../onboarding/onboarding_screen.dart';
 import '../welcome/welcome_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -14,21 +15,36 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.scheduleFrameCallback((_) {
-      context.read(settingProvider).read();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // _readSettingProvider();
+      context.read(settingProvider).readSettingProvider();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SplashScreenTemplate(
-        navigateAfterSplashScreen: (ctx) => WelcomeScreen(),
-        copyRightVersion: CopyRightVersion(),
-        image: ShowImageAsset(
-          imageUrl: "${appConfig.urlImageAsset}/${appConfig.urlLogoAsset}",
-          imageSize: 3,
-        ),
+      body: Consumer(
+        builder: (_, watch, __) {
+          // Karena namanya sama , gunakan alias
+          final setProvider = watch(settingProvider.state);
+
+          return SplashScreenTemplate(
+            navigateAfterSplashScreen: (ctx) {
+              /// Check if user already pass OnboardingScreen
+              if (setProvider.isAlreadyOnboardingScreen) {
+                return WelcomeScreen();
+              }
+              return OnboardingScreen();
+            },
+            copyRightVersion: CopyRightVersion(),
+            image: ShowImageAsset(
+              imageUrl: "${appConfig.urlImageAsset}/${appConfig.urlLogoAsset}",
+              imageSize: 3,
+            ),
+          );
+        },
       ),
     );
   }
