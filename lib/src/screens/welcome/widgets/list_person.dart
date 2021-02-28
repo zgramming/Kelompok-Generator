98bf215d4205771bfd16fa948e2df3965b302b08
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-02-28 16:36:32 +0700

    change behaviour from click => swipe to delete

--- lib/src/screens/welcome/widgets/list_person.dart
+++ lib/src/screens/welcome/widgets/list_person.dart
@@ -5,63 +5,101 @@ import 'package:global_template/global_template.dart';
 import '../../../functions/functions.dart';
 import '../../../network/models/models.dart';
 import '../../../providers/providers.dart';
+import 'form_update_name_person.dart';
 
 class ListPerson extends StatelessWidget {
   const ListPerson({
     Key key,
-    @required this.newIndex,
+    @required this.index,
     @required this.person,
     @required this.animation,
   }) : super(key: key);
 
-  final int newIndex;
+  final int index;
   final PersonModel person;
   final Animation<double> animation;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SlideTransition(
-          position: animation.drive(
-            Tween(begin: const Offset(1.0, 0.0), end: Offset.zero),
-          ),
-          child: ListTile(
-            leading: CircleAvatar(
-              child: Text("$newIndex"),
-            ),
-            title: Text(person.name),
-            trailing: Consumer(
-              builder: (context, watch, child) {
-                final keyAnimatedList = watch(globalAnimatedListKey).state;
-                return KeyboardVisibilityBuilder(
-                  builder: (context, child, isKeyboardVisible) {
-                    return AnimatedSwitcher(
-                      duration: const Duration(milliseconds: 500),
-                      child: isKeyboardVisible
-                          ? SizedBox()
-                          : ActionCircleButton(
-                              backgroundColor: colorPallete.red,
-                              icon: Icons.delete,
-                              foregroundColor: colorPallete.white,
-                              radius: sizes.isMobileLayout(context)
-                                  ? sizes.width(context) * .045
-                                  : sizes.width(context) * .025,
-                              onTap: () => FunctionRequest.deletePerson(
-                                context,
-                                animatedListKey: keyAnimatedList,
-                                index: newIndex - 1,
-                              ),
-                            ),
-                    );
-                  },
-                  child: child,
-                );
-              },
-            ),
-          ),
+    return SlideTransition(
+      position: animation.drive(
+        Tween(
+          begin: const Offset(1.0, 0.0),
+          end: Offset.zero,
         ),
-        Divider(color: colorPallete.accentColor),
-      ],
+      ),
+      child: Consumer(
+        builder: (_, watch, __) {
+          final animatedListKey = watch(globalAnimatedListKey).state;
+          return Dismissible(
+            key: UniqueKey(),
+            background: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.delete_rounded,
+                    color: colorPallete.white,
+                  ),
+                  Icon(
+                    Icons.delete_rounded,
+                    color: colorPallete.white,
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ),
+              color: colorPallete.red,
+            ),
+            onDismissed: (direction) {
+              FunctionRequest.deletePerson(
+                context,
+                animatedListKey: animatedListKey,
+                index: index,
+              );
+            },
+            child: ListTile(
+              onTap: () => showDialog(
+                context: context,
+                builder: (ctx) => FormUpdateNamePerson(id: person.id),
+              ),
+              contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 12.0),
+              leading: Container(
+                padding: const EdgeInsets.all(8),
+                width: sizes.width(context) / 15,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: colorPallete.accentColor,
+                  border: Border.all(color: colorPallete.accentColor),
+                ),
+                child: FittedBox(
+                  child: Text(
+                    "${index + 1}",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: colorPallete.white,
+                    ),
+                  ),
+                ),
+              ),
+              title: Text(
+                person.name,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              trailing: Container(
+                width: 2.5,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: colorPallete.red,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
