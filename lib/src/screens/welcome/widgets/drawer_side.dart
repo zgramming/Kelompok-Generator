bf845845aee8d57614fb2888a4e59ca3bca8a395
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-03-01 22:08:25 +0700

    feature rate app & copyright icon

--- lib/src/screens/welcome/widgets/drawer_side.dart
+++ lib/src/screens/welcome/widgets/drawer_side.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:global_template/global_template.dart';
+import 'package:launch_review/launch_review.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../history/history_screen.dart';
 import '../../../providers/providers.dart';
@@ -11,9 +14,18 @@ class DrawerSide extends StatelessWidget {
     Key key,
   }) : super(key: key);
 
+  Future<void> launchUrl(String url) async {
+    try {
+      if (await canLaunch(url)) {
+        await launch(url);
+      }
+    } catch (e) {
+      GlobalFunction.showToast(message: e.toString(), toastType: ToastType.error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    print(sizes.isMobileLayout(context));
     return Drawer(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -75,11 +87,93 @@ class DrawerSide extends StatelessWidget {
                   DrawerMenu(
                     showDivider: true,
                     trailing: Icon(
+                      Icons.copyright,
+                    ),
+                    title: 'Icon Copyright',
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (ctx) => AlertDialog(
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  ShowImageAsset(
+                                      imageUrl: '${appConfig.urlImageAsset}/freepik.png',
+                                      imageSize: 5),
+                                  ShowImageAsset(
+                                      imageUrl: '${appConfig.urlImageAsset}/flaticon.png',
+                                      imageSize: 5),
+                                ],
+                              ),
+                              RichText(
+                                textAlign: TextAlign.center,
+                                text: TextSpan(
+                                  text: 'Icons made by ',
+                                  style: TextStyle(color: colorPallete.black),
+                                  children: [
+                                    TextSpan(
+                                      text: 'Freepik ',
+                                      recognizer: TapGestureRecognizer()
+                                        ..onTap = () async {
+                                          await launchUrl('https://www.freepik.com');
+                                        },
+                                      style: TextStyle(
+                                        color: colorPallete.blue,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(text: 'from '),
+                                    TextSpan(
+                                      text: 'www.flaticon.com',
+                                      recognizer: TapGestureRecognizer()
+                                        ..onTap = () async {
+                                          await launchUrl('https://www.flaticon.com/');
+                                        },
+                                      style: TextStyle(
+                                        color: colorPallete.blue,
+                                        fontWeight: FontWeight.bold,
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  DrawerMenu(
+                    showDivider: true,
+                    trailing: Icon(
+                      Icons.star_border_rounded,
+                    ),
+                    title: 'Berikan rating',
+                    onTap: () {
+                      LaunchReview.launch(
+                        androidAppId: 'reynando.zeffry.kelompok_generator',
+                      );
+                    },
+                  ),
+                  DrawerMenu(
+                    showDivider: true,
+                    trailing: Icon(
                       Icons.mobile_friendly,
                     ),
                     title: 'Tentang aplikasi',
                     onTap: () {
-                      showLicensePage(context: context);
+                      showLicensePage(
+                        context: context,
+                        applicationIcon: ShowImageAsset(
+                          imageUrl: '${appConfig.urlImageAsset}/${appConfig.urlLogoAsset}',
+                          imageSize: 2,
+                        ),
+                      );
                     },
                   ),
                 ],
